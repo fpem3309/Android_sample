8489d179c4c784fda2c76f7eabfe6cbcb3e60c17
fpem3309 <alsrhks2154@gmail.com>
2021-12-21 02:26:09 +0900

    toast_massage

--- app/src/main/java/com/example/sampleapp/MainActivity.java
+++ app/src/main/java/com/example/sampleapp/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.sampleapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button go_login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        go_login = findViewById(R.id.go_login);
+
+
+        go_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Go Login Layout",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
