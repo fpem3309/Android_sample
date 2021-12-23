48556957b3c2581bd3c7730129374f2577f8ea92
fpem3309 <alsrhks2154@gmail.com>
2021-12-23 23:21:42 +0900

    intent

--- app/src/main/java/com/example/sampleapp/LoginActivity.java
+++ app/src/main/java/com/example/sampleapp/LoginActivity.java
@@ -1,30 +1,27 @@
 package com.example.sampleapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
 
     Button go_login;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
         go_login = findViewById(R.id.go_login);
 
         go_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                 startActivity(intent);
-                Toast.makeText(getApplicationContext(),"Go Login Layout",Toast.LENGTH_SHORT).show();
             }
         });
     }
