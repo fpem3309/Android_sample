6c1dda6671c0aa368f350e4b3af460f1bb00dc1e
fpem3309 <alsrhks2154@gmail.com>
2021-12-29 17:09:31 +0900

    intent_login

--- app/src/main/java/com/example/sampleapp/LoginActivity.java
+++ app/src/main/java/com/example/sampleapp/LoginActivity.java
@@ -4,26 +4,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.textfield.TextInputEditText;
+
 public class LoginActivity extends AppCompatActivity {
 
-    Button go_login;
+    RelativeLayout RelativeLayout_login;
+    TextInputEditText TextInputEditText_email, TextInputEditText_password;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        go_login = findViewById(R.id.go_main);
 
-        go_login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
-                finish(); // 현재 Activity종료 -> task에서 삭제
-            }
-        });
     }
 }
--- app/src/main/java/com/example/sampleapp/MainActivity.java
+++ app/src/main/java/com/example/sampleapp/MainActivity.java
@@ -6,20 +6,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.RelativeLayout;
 import android.widget.Toast;
-
-import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    RelativeLayout RelativeLayout_login;
-    TextInputEditText TextInputEditText_email, TextInputEditText_password;
+    Button go_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        go_login = findViewById(R.id.go_login);
+
+        go_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                Toast.makeText(getApplicationContext(),"Go Login Layout",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
