b3e156f208d1ffe195b63d1a7be07e1480447c68
fpem3309 <alsrhks2154@gmail.com>
2021-12-29 17:04:00 +0900

    theme_layout

--- app/src/main/java/com/example/sampleapp/MainActivity.java
+++ app/src/main/java/com/example/sampleapp/MainActivity.java
@@ -6,26 +6,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button go_login;
+    RelativeLayout RelativeLayout_login;
+    TextInputEditText TextInputEditText_email, TextInputEditText_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        go_login = findViewById(R.id.go_login);
-
-        go_login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
-                Toast.makeText(getApplicationContext(),"Go Login Layout",Toast.LENGTH_SHORT).show();
-            }
-        });
     }
 }
