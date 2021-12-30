f4d982b33d0095d16850d7389986996f0f70e9b0
fpem3309 <alsrhks2154@gmail.com>
2021-12-30 21:20:37 +0900

    intent_eamilpass

--- app/src/main/java/com/example/sampleapp/LoginActivity.java
+++ app/src/main/java/com/example/sampleapp/LoginActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,15 +13,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.textfield.TextInputEditText;
 
 public class LoginActivity extends AppCompatActivity {
-
-    RelativeLayout RelativeLayout_login;
-    TextInputEditText TextInputEditText_email, TextInputEditText_password;
+TextView get_email;
+TextView get_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        get_email = findViewById(R.id.get_email);
+        get_password = findViewById(R.id.get_password);
 
+        Intent intent = getIntent();
+
+        Bundle bundle = intent.getExtras();
+        String email = bundle.getString("email");
+        String password = bundle.getString("password");
+
+        get_email.setText(email);
+        get_password.setText(password);
     }
 }
--- app/src/main/java/com/example/sampleapp/MainActivity.java
+++ app/src/main/java/com/example/sampleapp/MainActivity.java
@@ -4,28 +4,44 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button go_login;
+    RelativeLayout RelativeLayout_login;
+    TextInputEditText TextInputEditText_email, TextInputEditText_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        go_login = findViewById(R.id.go_login);
+        TextInputEditText_email = findViewById(R.id.TextInputEditText_email);
+        TextInputEditText_password = findViewById(R.id.TextInputEditText_password);
+        RelativeLayout_login = findViewById(R.id.RelativeLayout_login);
 
-        go_login.setOnClickListener(new View.OnClickListener() {
+        // Login 클릭 감지
+        RelativeLayout_login.setClickable(true);
+        RelativeLayout_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                // 값 가져오기
+                String email = TextInputEditText_email.getText().toString();
+                String password = TextInputEditText_password.getText().toString();
+
                 Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                intent.putExtra("email",email);
+                intent.putExtra("password",password);
                 startActivity(intent);
-                Toast.makeText(getApplicationContext(),"Go Login Layout",Toast.LENGTH_SHORT).show();
             }
         });
+
     }
 }
