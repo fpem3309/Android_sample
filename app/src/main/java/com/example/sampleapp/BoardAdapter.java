4463f447ccdbe281ac4c39ce8800ac2bf2a48cf4
fpem3309 <alsrhks2154@gmail.com>
2022-01-26 16:42:57 +0900

    create_BoardAdapter

---
+++ app/src/main/java/com/example/sampleapp/BoardAdapter.java
@@ -0,0 +1,55 @@
+package com.example.sampleapp;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class BoardAdapter extends RecyclerView.Adapter<BoardAdapter.ViewHolder> {
+    private String[] localDataSet;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView textView;
+
+        public ViewHolder(View view) {
+            super(view);
+            // Define click listener for the ViewHolder's View
+
+            textView = (TextView) view.findViewById(R.id.textView);
+        }
+
+    }
+
+    public BoardAdapter(String[] dataSet) {
+        localDataSet = dataSet;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        // Create a new view, which defines the UI of the list item
+        View view = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.board_list, viewGroup, false);
+
+        return new ViewHolder(view);
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, final int position) {
+
+        // Get element from your dataset at this position and replace the
+        // contents of the view with that element
+
+        viewHolder.textView.setText(position);
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return localDataSet.length;
+    }
+}
